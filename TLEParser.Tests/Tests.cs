bdf360a7fddc0c3695e3f9a21590e95849860688
Richard Astbury <richard.astbury@gmail.com>
2019-03-14 20:31:39 +1100

    reformatted

--- TLEParser.Tests/Tests.cs
+++ TLEParser.Tests/Tests.cs
@@ -5,75 +5,75 @@ using System;
 
 namespace TLEParser.Tests
 {
-    [TestClass]
-    public class TLEParserTests
+  [TestClass]
+  public class TLEParserTests
+  {
+    [TestMethod]
+    public void ParseBasicString()
     {
-        [TestMethod]
-        public void ParseBasicString()
-        {
-            var iss = @"ISS (ZARYA)             
+      var iss = @"ISS (ZARYA)             
 1 25544U 98067A   18298.51635846  .00001514  00000-0  67960-4 0  9998
 2 25544  51.6406  89.2479 0003892 336.3122 134.3245 15.53861856138782";
 
-            var tle = TLEParser.TLE.Parse(iss);
+      var tle = TLEParser.TLE.Parse(iss);
 
-            Assert.AreEqual("ISS (ZARYA)", tle.Name);
-            Assert.AreEqual("98067A", tle.InternationalDesignator);
-            Assert.AreEqual(0.00001514m, tle.FirstDerivativeOfMeanMotion);
-            Assert.AreEqual(0.0m, tle.SecondDerivativeOfMeanMotion);
-            Assert.AreEqual(0.00006796m, tle.Drag);
-            Assert.AreEqual(0, tle.EphemerisType);
-            Assert.AreEqual(25544, tle.Number);
-            Assert.AreEqual(51.6406m, tle.Inclination);
-            Assert.AreEqual(89.2479m, tle.RightAscensionOfTheAscendingNode);
-            Assert.AreEqual(0.0003892m, tle.Eccentricity);
-            Assert.AreEqual(336.3122m, tle.ArgumentOfPerigee);
-            Assert.AreEqual(134.3245m, tle.MeanAnomaly); 
-            Assert.AreEqual(15.53861856m, tle.MeanMotion);
-            Assert.AreEqual(13878, tle.Revolutions);
-        }
+      Assert.AreEqual("ISS (ZARYA)", tle.Name);
+      Assert.AreEqual("98067A", tle.InternationalDesignator);
+      Assert.AreEqual(0.00001514m, tle.FirstDerivativeOfMeanMotion);
+      Assert.AreEqual(0.0m, tle.SecondDerivativeOfMeanMotion);
+      Assert.AreEqual(0.00006796m, tle.Drag);
+      Assert.AreEqual(0, tle.EphemerisType);
+      Assert.AreEqual(25544, tle.Number);
+      Assert.AreEqual(51.6406m, tle.Inclination);
+      Assert.AreEqual(89.2479m, tle.RightAscensionOfTheAscendingNode);
+      Assert.AreEqual(0.0003892m, tle.Eccentricity);
+      Assert.AreEqual(336.3122m, tle.ArgumentOfPerigee);
+      Assert.AreEqual(134.3245m, tle.MeanAnomaly);
+      Assert.AreEqual(15.53861856m, tle.MeanMotion);
+      Assert.AreEqual(13878, tle.Revolutions);
+    }
 
-        public void ParseExampleFromGitHub()
-        {
-             var lines = new string[]{
+    public void ParseExampleFromGitHub()
+    {
+      var lines = new string[]{
                 "ISS (ZARYA)",
                 "1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2927",
                 "2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563537"
             };
 
-            var tle = TLEParser.TLE.Parse(lines[0], lines[1], lines[2]);
+      var tle = TLEParser.TLE.Parse(lines[0], lines[1], lines[2]);
 
-            Assert.AreEqual("ISS (ZARYA)", tle.Name);
-            Assert.AreEqual("98067A", tle.InternationalDesignator);
-            Assert.AreEqual(-0.00002182m, tle.FirstDerivativeOfMeanMotion);
-            Assert.AreEqual(0.0m, tle.SecondDerivativeOfMeanMotion);
-            Assert.AreEqual(-0.000011606m, tle.Drag);
-            Assert.AreEqual(new DateTime(2008, 8, 20, 12, 2, 40, 1040), tle.Date);
-            Assert.AreEqual(0, tle.EphemerisType);
-            Assert.AreEqual(25544, tle.Number);
-            Assert.AreEqual(51.6416m, tle.Inclination);
-            Assert.AreEqual(247.4627m, tle.RightAscensionOfTheAscendingNode);
-            Assert.AreEqual(0.0006703m, tle.Eccentricity);
-            Assert.AreEqual(130.536m, tle.ArgumentOfPerigee);
-            Assert.AreEqual(325.0288m, tle.MeanAnomaly);
-            Assert.AreEqual(15.721253915m, tle.MeanMotion);
-            Assert.AreEqual(56353, tle.Revolutions);
-        }
-
-        [TestMethod]
-        public void TestParseNum()
-        {   
-            Assert.AreEqual(0.00001514m, TLEParser.TLE.ParseNum(".00001514"));
-            Assert.AreEqual(-0.02m, TLEParser.TLE.ParseNum("-.02"));
-        }
-
-        [TestMethod]
-        public void TestParseNumWithPower()
-        {   
-            Assert.AreEqual(0.0m, TLEParser.TLE.ParseNumWithPower("0.0000-0"));
-            Assert.AreEqual(0.01m, TLEParser.TLE.ParseNumWithPower("1.0000-2"));
-        }
-
-
+      Assert.AreEqual("ISS (ZARYA)", tle.Name);
+      Assert.AreEqual("98067A", tle.InternationalDesignator);
+      Assert.AreEqual(-0.00002182m, tle.FirstDerivativeOfMeanMotion);
+      Assert.AreEqual(0.0m, tle.SecondDerivativeOfMeanMotion);
+      Assert.AreEqual(-0.000011606m, tle.Drag);
+      Assert.AreEqual(new DateTime(2008, 8, 20, 12, 2, 40, 1040), tle.Date);
+      Assert.AreEqual(0, tle.EphemerisType);
+      Assert.AreEqual(25544, tle.Number);
+      Assert.AreEqual(51.6416m, tle.Inclination);
+      Assert.AreEqual(247.4627m, tle.RightAscensionOfTheAscendingNode);
+      Assert.AreEqual(0.0006703m, tle.Eccentricity);
+      Assert.AreEqual(130.536m, tle.ArgumentOfPerigee);
+      Assert.AreEqual(325.0288m, tle.MeanAnomaly);
+      Assert.AreEqual(15.721253915m, tle.MeanMotion);
+      Assert.AreEqual(56353, tle.Revolutions);
     }
+
+    [TestMethod]
+    public void TestParseNum()
+    {
+      Assert.AreEqual(0.00001514m, TLEParser.TLE.ParseNum(".00001514"));
+      Assert.AreEqual(-0.02m, TLEParser.TLE.ParseNum("-.02"));
+    }
+
+    [TestMethod]
+    public void TestParseNumWithPower()
+    {
+      Assert.AreEqual(0.0m, TLEParser.TLE.ParseNumWithPower("0.0000-0"));
+      Assert.AreEqual(0.01m, TLEParser.TLE.ParseNumWithPower("1.0000-2"));
+    }
+
+
+  }
 }
--- TLEParser/TLE.cs
+++ TLEParser/TLE.cs
@@ -3,103 +3,103 @@ using System.Linq;
 
 namespace TLEParser
 {
-    public class TLE
+  public class TLE
+  {
+    private TLE()
+    { }
+
+    public string Name { get; private set; }
+    public int Number { get; private set; }
+    public string InternationalDesignator { get; private set; }
+    public decimal FirstDerivativeOfMeanMotion { get; private set; }
+    public decimal SecondDerivativeOfMeanMotion { get; private set; }
+    public decimal Drag { get; private set; }
+    public decimal EphemerisType { get; private set; }
+    public decimal Inclination { get; private set; }
+    public decimal RightAscensionOfTheAscendingNode { get; private set; }
+    public decimal Eccentricity { get; private set; }
+    public decimal ArgumentOfPerigee { get; private set; }
+    public decimal MeanAnomaly { get; private set; }
+    public decimal MeanMotion { get; private set; }
+    public DateTime Date { get; private set; }
+    public int Revolutions { get; private set; }
+
+    public static TLE Parse(string line0, string line1, string line2)
     {
-        private TLE()
-        {}
+      var tle = new TLE();
 
-        public string Name { get; private set; }
-        public int Number { get; private set; }
-        public string InternationalDesignator { get; private set; }
-        public decimal FirstDerivativeOfMeanMotion { get; private set; }
-        public decimal SecondDerivativeOfMeanMotion { get; private set; }
-        public decimal Drag { get; private set; }
-        public decimal EphemerisType { get; private set; }
-        public decimal Inclination { get; private set; }
-        public decimal RightAscensionOfTheAscendingNode { get; private set; }
-        public decimal Eccentricity { get; private set; }
-        public decimal ArgumentOfPerigee { get; private set; }
-        public decimal MeanAnomaly { get; private set; }
-        public decimal MeanMotion { get; private set; }
-        public DateTime Date { get; private set; }
-        public int Revolutions { get; private set; }
+      // line 0
+      tle.Name = line0.Trim();
 
-        public static TLE Parse(string line0, string line1, string line2)
-        {
-            var tle = new TLE();
+      // line 1
+      var line1Number = line1.Substring(0, 1);
+      if (line1Number != "1") throw new FormatException("Line 1 should start with a '1'");
 
-            // line 0
-            tle.Name = line0.Trim();
+      tle.InternationalDesignator = line1.Substring(9, 8).Trim();
 
-            // line 1
-            var line1Number = line1.Substring(0,1);
-            if (line1Number != "1") throw new FormatException("Line 1 should start with a '1'");
+      var twoDigitYear = int.Parse(line1.Substring(18, 2).Trim());
+      var dayFraction = ParseNum(line1.Substring(20, 12));
+      var days = Math.Floor(dayFraction);
+      var hoursFraction = 24 * (dayFraction - days);
+      var hours = Math.Floor(hoursFraction);
+      var minutesFraction = 60 * (hoursFraction - hours);
+      var minutes = Math.Floor(minutesFraction);
+      var secondsFraction = 60 * (minutesFraction - minutes);
+      var seconds = Math.Floor(secondsFraction);
+      var milliseconds = 1000 * (secondsFraction - seconds);
 
-            tle.InternationalDesignator = line1.Substring(9, 8).Trim();
+      tle.Date = new DateTime(2000 + twoDigitYear, 1, 1)
+          .AddDays((int)days)
+          .AddHours((int)hours)
+          .AddMinutes((int)minutes)
+          .AddSeconds((int)seconds)
+          .AddMilliseconds((int)milliseconds);
 
-            var twoDigitYear = int.Parse(line1.Substring(18, 2).Trim());
-            var dayFraction = ParseNum(line1.Substring(20, 12));
-            var days = Math.Floor(dayFraction);
-            var hoursFraction = 24 * (dayFraction - days);
-            var hours = Math.Floor(hoursFraction);
-            var minutesFraction = 60 * (hoursFraction - hours);
-            var minutes = Math.Floor(minutesFraction);
-            var secondsFraction = 60 * (minutesFraction - minutes); 
-            var seconds = Math.Floor(secondsFraction);
-            var milliseconds = 1000 * (secondsFraction - seconds);
+      tle.FirstDerivativeOfMeanMotion = ParseNum(line1.Substring(33, 10));
+      tle.SecondDerivativeOfMeanMotion = ParseNumWithPower("0." + line1.Substring(44, 6).Trim());
+      tle.Drag = ParseNumWithPower("0." + line1.Substring(53, 8).Trim());
+      tle.EphemerisType = int.Parse(line1.Substring(62, 2).Trim());
 
-            tle.Date = new DateTime(2000 + twoDigitYear, 1, 1)
-                .AddDays((int) days)
-                .AddHours((int) hours)
-                .AddMinutes((int) minutes)
-                .AddSeconds((int) seconds)
-                .AddMilliseconds((int) milliseconds);
+      // line 2
+      var line2Number = line2.Substring(0, 1);
+      if (line2Number != "2") throw new FormatException("Line 2 should start with a '2'");
 
-            tle.FirstDerivativeOfMeanMotion = ParseNum(line1.Substring(33, 10));
-            tle.SecondDerivativeOfMeanMotion = ParseNumWithPower("0." + line1.Substring(44, 6).Trim());
-            tle.Drag = ParseNumWithPower("0." + line1.Substring(53, 8).Trim());
-            tle.EphemerisType = int.Parse(line1.Substring(62, 2).Trim());  
-            
-            // line 2
-            var line2Number = line2.Substring(0,1);
-            if (line2Number != "2") throw new FormatException("Line 2 should start with a '2'");
+      tle.Number = int.Parse(line2.Substring(2, 6).Trim());
+      tle.Inclination = ParseNum(line2.Substring(9, 7));
+      tle.RightAscensionOfTheAscendingNode = ParseNum(line2.Substring(17, 8));
+      tle.Eccentricity = ParseNum("0." + line2.Substring(26, 7));
+      tle.ArgumentOfPerigee = ParseNum(line2.Substring(34, 8));
+      tle.MeanAnomaly = ParseNum(line2.Substring(43, 8));
+      tle.MeanMotion = ParseNum(line2.Substring(52, 11));
+      tle.Revolutions = int.Parse(line2.Substring(63, 5).Trim());
 
-            tle.Number = int.Parse(line2.Substring(2, 6).Trim());
-            tle.Inclination = ParseNum(line2.Substring(9,7));
-            tle.RightAscensionOfTheAscendingNode = ParseNum(line2.Substring(17, 8));
-            tle.Eccentricity = ParseNum("0." + line2.Substring(26, 7));
-            tle.ArgumentOfPerigee = ParseNum(line2.Substring(34, 8));
-            tle.MeanAnomaly = ParseNum(line2.Substring(43, 8));
-            tle.MeanMotion = ParseNum(line2.Substring(52, 11));
-            tle.Revolutions = int.Parse(line2.Substring(63, 5).Trim());
-
-            return tle;
-        }
-
-        public static TLE Parse(string value)
-        {
-            if (string.IsNullOrWhiteSpace(value)) throw new ArgumentNullException(nameof(value));
-
-            var lines = value.Split('\n').Select(x => x.Trim()).ToArray();
-
-            if (lines.Length != 3) throw new FormatException("Expected 3 lines");
-
-            return Parse(lines[0], lines[1], lines[2]);
-        }
-
-        public static decimal ParseNum(string value)
-        {
-            if (string.IsNullOrWhiteSpace(value)) return 0m;
-            return decimal.Parse(value.Trim());
-        }
-
-        public static decimal ParseNumWithPower(string value)
-        {
-            var num = ParseNum(value.Substring(0, value.Length - 2));
-            var pow = ParseNum(value.Substring(value.Length - 2, 2));
-            
-            return num * (decimal) Math.Pow(10, (double) pow); 
-        }
-
+      return tle;
     }
+
+    public static TLE Parse(string value)
+    {
+      if (string.IsNullOrWhiteSpace(value)) throw new ArgumentNullException(nameof(value));
+
+      var lines = value.Split('\n').Select(x => x.Trim()).ToArray();
+
+      if (lines.Length != 3) throw new FormatException("Expected 3 lines");
+
+      return Parse(lines[0], lines[1], lines[2]);
+    }
+
+    public static decimal ParseNum(string value)
+    {
+      if (string.IsNullOrWhiteSpace(value)) return 0m;
+      return decimal.Parse(value.Trim());
+    }
+
+    public static decimal ParseNumWithPower(string value)
+    {
+      var num = ParseNum(value.Substring(0, value.Length - 2));
+      var pow = ParseNum(value.Substring(value.Length - 2, 2));
+
+      return num * (decimal)Math.Pow(10, (double)pow);
+    }
+
+  }
 }
